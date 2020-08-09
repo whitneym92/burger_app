5eef4e08df0817c8c3c54fcaf25421d9c4606b95
Whitney Monk <whitneym92@gmail.com>
2020-08-09 18:53:01 -0500

    added burgers into my seed.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Peas and Thank You Burger');
+INSERT INTO burgers (burger_name) VALUES ('Dont You Four Cheddar Bout Me Burger');
+INSERT INTO burgers (burger_name) VALUES ('Onion Ring Around the Rosemary Burger');
+INSERT INTO burgers (burger_name) VALUES ('Open Sesame Burger');
+INSERT INTO burgers (burger_name) VALUES ('Blones Have More Fun-gus Burger(comes with mushrooms)');
