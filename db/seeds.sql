ca886abafac7e3a75e8e1bbbc425c26fd675e087
Whitney Monk <whitneym92@gmail.com>
2020-08-09 23:27:02 -0500

    fixed a spelling error

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO burgers (burger_name) VALUES ('Peas and Thank You Burger');
 INSERT INTO burgers (burger_name) VALUES ('Dont You Four Cheddar Bout Me Burger');
 INSERT INTO burgers (burger_name) VALUES ('Onion Ring Around the Rosemary Burger');
 INSERT INTO burgers (burger_name) VALUES ('Open Sesame Burger');
-INSERT INTO burgers (burger_name) VALUES ('Blones Have More Fun-gus Burger(comes with mushrooms)');
+INSERT INTO burgers (burger_name) VALUES ('Blondes Have More Fun-gus Burger(comes with mushrooms)');
