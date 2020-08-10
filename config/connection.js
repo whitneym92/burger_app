a8cd344317231bc2191d3abe4e7b6e092218d642
Whitney Monk <whitneym92@gmail.com>
2020-08-09 22:24:08 -0500

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -14,14 +14,15 @@ else { connection = mysql.createConnection({
 });
 };
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+// // Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // Export connection for our ORM to use.
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -9,3 +9,8 @@ CREATE TABLE burgers
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+INSERT INTO burgers (burger_name) VALUES ('Peas and Thank You Burger');
+INSERT INTO burgers (burger_name) VALUES ('Dont You Four Cheddar Bout Me Burger');
+INSERT INTO burgers (burger_name) VALUES ('Onion Ring Around the Rosemary Burger');
+INSERT INTO burgers (burger_name) VALUES ('Open Sesame Burger');
+INSERT INTO burgers (burger_name) VALUES ('Blones Have More Fun-gus Burger(comes with mushrooms)');
