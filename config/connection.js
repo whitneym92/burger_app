627570a802e616d7afbbdc910e11d691d07afe0e
Whitney Monk <whitneym92@gmail.com>
2020-08-09 22:45:07 -0500

    updated the connection.js

--- config/connection.js
+++ config/connection.js
@@ -14,15 +14,15 @@ else { connection = mysql.createConnection({
 });
 };
 
-connection.connect();
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // Export connection for our ORM to use.
 module.exports = connection;
